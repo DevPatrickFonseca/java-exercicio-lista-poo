8da09024da83bd47d52a5ff78f1de9388e3d808d
PFonsecaFV <pfonsecafv@gmail.com>
2023-09-25 18:23:13 -0300

    ✨ Req 01 - Sistema de Recursos Humanos - PessoaFuncionaria

--- src/main/java/com/trybe/acc/recursoshumanos/PessoaFuncionaria.java
+++ src/main/java/com/trybe/acc/recursoshumanos/PessoaFuncionaria.java
@@ -5,4 +5,57 @@ package com.trybe.acc.recursoshumanos;
  */
 public class PessoaFuncionaria {
 
+  private String nomeCompleto;
+  private String cpf;
+  private String endereco;
+  private double salario;
+
+  /**
+   * Construtor da classe PessoaFuncionaria.
+   */
+
+  public PessoaFuncionaria(
+      String nomeCompleto,
+      String cpf,
+      String endereco,
+      double salario
+  ) {
+    this.nomeCompleto = nomeCompleto;
+    this.cpf = cpf;
+    this.endereco = endereco;
+    this.salario = salario;
+  }
+
+  // Getters e Setters
+  public String getNomeCompleto() {
+    return nomeCompleto;
+  }
+
+  public void setNomeCompleto(String nomeCompleto) {
+    this.nomeCompleto = nomeCompleto;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
+
+  public String getEndereco() {
+    return endereco;
+  }
+
+  public void setEndereco(String endereco) {
+    this.endereco = endereco;
+  }
+
+  public double getSalario() {
+    return salario;
+  }
+
+  public void setSalario(double salario) {
+    this.salario = salario;
+  }
 }
