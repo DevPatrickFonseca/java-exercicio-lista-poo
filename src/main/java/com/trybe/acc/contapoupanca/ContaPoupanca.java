0d698afbf5ab67c7cc4444e3097a8342ba53222f
PFonsecaFV <pfonsecafv@gmail.com>
2023-09-25 18:24:21 -0300

    ✨ Req 02 - Conta Poupança - ContaPoupanca

--- src/main/java/com/trybe/acc/contapoupanca/ContaPoupanca.java
+++ src/main/java/com/trybe/acc/contapoupanca/ContaPoupanca.java
@@ -1,5 +1,33 @@
 package com.trybe.acc.contapoupanca;
 
+
 public class ContaPoupanca {
 
+  private String titularConta;
+  private double saldo;
+
+  /**
+   * Construtor da classe ContaPoupanca.
+   */
+  public ContaPoupanca(String titularConta, double saldo) {
+    this.titularConta = titularConta;
+    this.saldo = saldo;
+  }
+
+  // Métodos depositar, sacar, mostrarSaldo e mostrarTitularConta
+  public void depositar(double valor) {
+    this.saldo += valor;
+  }
+
+  public void sacar(double valor) {
+    this.saldo -= valor;
+  }
+
+  public double mostrarSaldo() {
+    return this.saldo;
+  }
+
+  public String mostrarTitularConta() {
+    return this.titularConta;
+  }
 }
