96af5e7bbeb37b2a2fd288ea9dc7bcdffd3dac99
PFonsecaFV <pfonsecafv@gmail.com>
2023-09-25 18:25:45 -0300

    ✨ Req 03 - Pessoa Usuárias - Pessoa PessoaUsuaria

--- src/main/java/com/trybe/acc/pessoausuaria/PessoaUsuaria.java
+++ src/main/java/com/trybe/acc/pessoausuaria/PessoaUsuaria.java
@@ -2,15 +2,20 @@ package com.trybe.acc.pessoausuaria;
 
 public class PessoaUsuaria extends Pessoa {
 
-  public PessoaUsuaria() {
-
+  public PessoaUsuaria(String nome, String sobrenome) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
   }
 
   /**
    * Método para pegar o nome e sobrenome do usuário.
-   *
    */
   public String getPessoaUsuaria() {
-    return null;
+    if ((nome == null || nome.equals(""))
+        || (sobrenome == null || sobrenome.equals(""))) {
+      return "Pessoa usuária inválida";
+    } else {
+      return nome + "." + sobrenome;
+    }
   }
 }
